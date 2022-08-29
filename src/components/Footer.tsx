8223d95e08a881b392a43dbe022c593c25706830
さりえり <nakakou201250@gmail.com>
2022-08-30 01:17:02 +0900

    Clean up

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => {
-    return (
-        <>&copy;2022 salieri256</>
-    )
-}
-
-export default Footer
--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-const Header = () => {
-    return (
-        <>OpenSesame v1.0.0</>
-    )
-}
-
-export default Header
