3b83b5352a38dc5a131b9cfe1e89a3f80e9c6869
さりえり <nakakou201250@gmail.com>
2022-08-30 01:19:34 +0900

    Implement API Call

---
+++ src/libs/api.ts
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react"
+
+type User = {
+    id: number
+    name: string
+    isActive: boolean
+    nfcId: string
+}
+
+type Door = {
+    id: number
+    name: string
+    isLocked: boolean
+}
+
+type ActivityLog = {
+    id: number
+    unixTime: number
+    isActive: boolean
+    userId: number
+}
+
+type LockLog = {
+    id: number
+    unixTime: number
+    isLocked: boolean
+    doorId: number
+    userId: number
+}
+
+const useAPI = <T>(input: RequestInfo | URL, init?: RequestInit | undefined) => {
+    const [data, setData] = useState<T>()
+
+    useEffect(() => {
+        fetch(input, init)
+        .then<T>(res => res.json())
+        .then(object => setData(object))
+    }, [])
+
+    return data
+}
+
+const useUsers = () => {
+    return useAPI<User[]>('http://192.168.11.10:8000/users', {mode: 'cors'})
+}
+
+const useDoors = () => {
+    return useAPI<Door[]>('http://192.168.11.10:8000/doors', {mode: 'cors'})
+}
+
+const useActivityLogs = () => {
+    return useAPI<ActivityLog[]>('http://192.168.11.10:8000/logs/activities', {mode: 'cors'})
+}
+
+const useLockLogs = () => {
+    return useAPI<LockLog[]>('http://192.168.11.10:8000/logs/locks', {mode: 'cors'})
+}
+
+export { useUsers, useDoors, useActivityLogs, useLockLogs }
